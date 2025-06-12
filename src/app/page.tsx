282e9e63ccd2b1af5bc8a1a7d725192eb252cb9a
Evgeny Shmakov <sjs-master@yandex.ru>
2025-06-12 17:51:51 +0700

    add styled-components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Button from '@/components/Button'
 
 export default function Home() {
   return (
@@ -12,6 +13,9 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+
+        <Button>Кнопка</Button>
+        <Button primary>Кнопка primary</Button>
 
         <div className={styles.ctas}>
           <a
