642b0666d95f0ed7678f8fce5748ace176003d31
Evgeny Shmakov <sjs-master@yandex.ru>
2025-06-12 17:51:51 +0700

    add styled-components

---
+++ src/components/StyledComponentsRegistry.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React, { useState } from 'react'
+import { useServerInsertedHTML } from 'next/navigation'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+
+export interface StyledComponentsRegistryProps {
+  children: React.ReactNode
+}
+
+export default function StyledComponentsRegistry({ children }: Readonly<StyledComponentsRegistryProps>) {
+  const [sheet] = useState(() => new ServerStyleSheet())
+
+  useServerInsertedHTML(() => {
+    try {
+      const styles = sheet.getStyleElement()
+      return <>{styles}</>
+    } catch (error) {
+      console.error('Error processing styled-components styles:', error)
+      return null
+    } finally {
+      sheet.instance.clearTag()
+    }
+  })
+
+  if (typeof window !== 'undefined') return <>{children}</>
+
+  return <StyleSheetManager sheet={sheet.instance}>{children}</StyleSheetManager>
+}
