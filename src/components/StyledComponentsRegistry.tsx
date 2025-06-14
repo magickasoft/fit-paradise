ae06454a1df17bc63826b6e96a4f7d6ccb020aa0
Evgeny Shmakov <sjs-master@yandex.ru>
2025-06-14 20:08:42 +0700

    Update StyledComponentsRegistry.tsx

--- src/components/StyledComponentsRegistry.tsx
+++ src/components/StyledComponentsRegistry.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 
@@ -11,18 +11,13 @@ export interface StyledComponentsRegistryProps {
 export default function StyledComponentsRegistry({ children }: Readonly<StyledComponentsRegistryProps>) {
   const [sheet] = useState(() => new ServerStyleSheet())
 
-  useEffect(() => {
-    return () => {
-      sheet?.seal()
-      sheet?.instance?.clearTag()
-    }
-  }, [])
-
   useServerInsertedHTML(() => {
     const styles = sheet?.getStyleElement()
     sheet?.instance?.clearTag()
     return <>{styles}</>
   })
+
+  if (typeof window !== 'undefined') return <>{children}</>
 
   return <StyleSheetManager sheet={sheet?.instance}>{children}</StyleSheetManager>
 }
