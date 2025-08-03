8c9218e2d609cf0b5d400c8dda1dc189f2bc1a03
AtrVorobev <50021543+AtrVorobev@users.noreply.github.com>
2025-08-03 22:37:08 +0300

    fix (#73)

--- messages/ru.d.json.ts
+++ messages/ru.d.json.ts
@@ -2,27 +2,27 @@
 // See: https://next-intl.dev/docs/workflows/typescript#messages-arguments
 
 declare const messages: {
-  btn: {
-    default: {
-      label: 'Кнопка default'
+  "btn": {
+    "default": {
+      "label": "Кнопка default"
+    },
+    "primary": {
+      "label": "Кнопка primary"
     }
-    primary: {
-      label: 'Кнопка primary'
-    }
+  },
+  "localeSwitcher": {
+    "label": "Выберите язык",
+    "locale": "{locale, select, ru {Russia} en {English} other {Unknown}}"
+  },
+  "manifest": {
+    "name": "next-intl example"
+  },
+  "localeLayout": {
+    "title": "next-intl example"
+  },
+  "notFoundPage": {
+    "description": "Пожалуйста, еще раз проверьте адресную строку браузера или воспользуйтесь навигацией, чтобы перейти на известную страницу.",
+    "title": "Страница не найдена"
   }
-  localeSwitcher: {
-    label: 'Выберите язык'
-    locale: '{locale, select, ru {Russia} en {English} other {Unknown}}'
-  }
-  manifest: {
-    name: 'next-intl example'
-  }
-  localeLayout: {
-    title: 'next-intl example'
-  }
-  notFoundPage: {
-    description: 'Пожалуйста, еще раз проверьте адресную строку браузера или воспользуйтесь навигацией, чтобы перейти на известную страницу.'
-    title: 'Страница не найдена'
-  }
-}
-export default messages
+};
+export default messages;
