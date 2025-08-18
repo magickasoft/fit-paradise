de64cde878be853ab05d15bcd1869d335d72d41d
Evgeny <sjs-master@yandex.ru>
2025-08-19 03:15:36 +0700

    complexity (#102)

--- src/helpers/findByKey.ts
+++ src/helpers/findByKey.ts
@@ -1,8 +1,20 @@
 import { Recipe, RECIPES_OBJ } from '../constants/recipes/recipes'
 
-export const findByKey = async (key: string): Promise<Recipe | null> => {
-  const target = key.toLowerCase()
+const RECIPE_MAP = (() => {
+  const map = new Map<string, Recipe>()
+  for (const category of Object.values(RECIPES_OBJ)) {
+    for (const recipe of category) {
+      if (recipe.key) {
+        const normalizedKey = recipe.key.toLowerCase()
+        if (!map.has(normalizedKey)) {
+          map.set(normalizedKey, recipe)
+        }
+      }
+    }
+  }
+  return map
+})()
 
-  const allRecipes = Object.values(RECIPES_OBJ).flat()
-  return allRecipes.find(recipe => recipe.key?.toLowerCase() === target) ?? null
+export const findByKey = async (key: string): Promise<Recipe | null> => {
+  return RECIPE_MAP.get(key.toLowerCase()) ?? null
 }
