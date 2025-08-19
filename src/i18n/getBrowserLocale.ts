023aa7eaa1c7a76a54a1ad90d2c7dbc3e45467df
Evgeny Shmakov <sjs-master@yandex.ru>
2025-08-20 03:16:26 +0700

    Update getBrowserLocale.ts

--- src/i18n/getBrowserLocale.ts
+++ src/i18n/getBrowserLocale.ts
@@ -4,9 +4,7 @@ import { isSupportedLocale } from './isSupportedLocale'
 export const getBrowserLocale = (): SupportedLocale | null => {
   if (typeof navigator === 'undefined') return null
 
-  const supportedLanguage = navigator.languages
-    .map(lang => lang.split('-', 1)[0])
-    .find(isSupportedLocale)
+  const supportedLanguage = navigator.languages.map(lang => lang.split('-', 1)[0]).find(isSupportedLocale)
 
-  return supportedLanguage || null
+  return supportedLanguage ?? null
 }
