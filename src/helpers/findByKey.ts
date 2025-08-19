e9cf9e5dbcaedc0289929df6701b5782b9fec624
Evgeny Shmakov <sjs-master@yandex.ru>
2025-08-20 03:37:30 +0700

    Update findByKey.ts

--- src/helpers/findByKey.ts
+++ src/helpers/findByKey.ts
@@ -1,20 +1,33 @@
 import { Recipe, RECIPES_OBJ } from '../constants/recipes/recipes'
 
-const RECIPE_MAP = (() => {
+const RECIPE_MAP = createRecipeMap()
+
+function createRecipeMap(): Map<string, Recipe> {
   const map = new Map<string, Recipe>()
-  for (const category of Object.values(RECIPES_OBJ)) {
-    for (const recipe of category) {
-      if (recipe.key) {
-        const normalizedKey = recipe.key.toLowerCase()
-        if (!map.has(normalizedKey)) {
-          map.set(normalizedKey, recipe)
-        }
-      }
+
+  const allRecipes = getAllRecipes()
+
+  for (const recipe of allRecipes) {
+    if (!recipe.key) continue
+
+    const normalizedKey = normalizeKey(recipe.key)
+    if (!map.has(normalizedKey)) {
+      map.set(normalizedKey, recipe)
     }
   }
+
   return map
-})()
+}
+
+function getAllRecipes(): Recipe[] {
+  return Object.values(RECIPES_OBJ).flat()
+}
+
+function normalizeKey(key: string): string {
+  return key.toLowerCase()
+}
 
 export const findByKey = async (key: string): Promise<Recipe | null> => {
-  return RECIPE_MAP.get(key.toLowerCase()) ?? null
+  const normalizedKey = normalizeKey(key)
+  return RECIPE_MAP.get(normalizedKey) ?? null
 }
