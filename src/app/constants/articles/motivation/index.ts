4add6eecf92d58a65f1c3428abe3281af463e285
AtrVorobev <50021543+AtrVorobev@users.noreply.github.com>
2025-09-18 23:10:31 +0300

    Feature/#134 (#135)
    * add new
    
    * fix
    
    ---------
    
    Co-authored-by: AtrVorobev <a.vorobev@clussters.ru>

--- src/app/constants/articles/motivation/index.ts
+++ src/app/constants/articles/motivation/index.ts
@@ -1,7 +1,15 @@
+import kinoKakTrener5FilmovKotoryeZastavyatVasPolyubitSport from './kino-kak-trener-5-filmov-kotorye-zastavyat-vas-polyubit-sport.json'
+import kakPolyubitSvoeTeloPoNastoyashchemuPutKGarmoniiBezNasiliyaNadSoboy from './kak-polyubit-svoe-telo-po-nastoyashchemu-put-k-garmonii-bez-nasiliya-nad-soboy.json'
+
 export const motivationArticles = {
   id: 'motivation',
   title: 'Мотивация',
   img: '/static/sections-imgs/motivationArticles.jpeg',
   description: '',
-  content: {},
+  content: {
+    'kino-kak-trener-5-filmov-kotorye-zastavyat-vas-polyubit-sport':
+      kinoKakTrener5FilmovKotoryeZastavyatVasPolyubitSport,
+    'kak-polyubit-svoe-telo-po-nastoyashchemu-put-k-garmonii-bez-nasiliya-nad-soboy':
+      kakPolyubitSvoeTeloPoNastoyashchemuPutKGarmoniiBezNasiliyaNadSoboy,
+  },
 }
