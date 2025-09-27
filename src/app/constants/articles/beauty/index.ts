7bdfcf13c3eb98aa24e8fc38a5cf5e0da4ab60dc
AtrVorobev <50021543+AtrVorobev@users.noreply.github.com>
2025-09-27 20:39:56 +0300

    Feature/#138 (#141)
    * add new
    
    * fix
    
    ---------
    
    Co-authored-by: AtrVorobev <a.vorobev@clussters.ru>

--- src/app/constants/articles/beauty/index.ts
+++ src/app/constants/articles/beauty/index.ts
@@ -1,4 +1,5 @@
 import takLiEffektivenFeysfitnesKakONemGovoryat from './tak-li-effektiven-feysfitnes-kak-o-nem-govoryat.json'
+import figuraSkinniFatPochemuVesVNormeATeloNePodtyanuto from './figura-skinni-fat-pochemu-ves-v-norme-a-telo-ne-podtyanuto.json'
 
 export const beautyArticles = {
   id: 'beauty',
@@ -7,5 +8,6 @@ export const beautyArticles = {
   description: '',
   content: {
     'tak-li-effektiven-feysfitnes-kak-o-nem-govoryat': takLiEffektivenFeysfitnesKakONemGovoryat,
+    'figura-skinni-fat-pochemu-ves-v-norme-a-telo-ne-podtyanuto': figuraSkinniFatPochemuVesVNormeATeloNePodtyanuto,
   },
 }
--- src/app/constants/articles/health/index.ts
+++ src/app/constants/articles/health/index.ts
@@ -1,7 +1,17 @@
+import sportPosle50InvestitsiyaVZdoroveEnergiyuIMolodost from './sport-posle-50-investitsiya-v-zdorove-energiyu-i-molodost.json'
+import pochemuMyshechnayaMassaVazhnaNekojNoITolkoDlyaRelefaNoIuDlyaUskoreniyaMetabolizma from './pochemu-myshechnaya-massa-vazhna-ne-tolko-dlya-relefa-no-i-dlya-uskoreniya-metabolizma.json'
+import takLiPolezenBegRazbiraemsyaKakIzvlechMaksimumPolzyIIzbezhatVreda from './tak-li-polezen-beg-razbiraemsya-kak-izvlech-maksimum-polzy-i-izbezhat-vreda.json'
+
 export const healthArticles = {
   id: 'health',
   title: 'Здоровье',
   img: '/static/sections-imgs/healthArticles.jpeg',
   description: '',
-  content: {},
+  content: {
+    'sport-posle-50-investitsiya-v-zdorove-energiyu-i-molodost': sportPosle50InvestitsiyaVZdoroveEnergiyuIMolodost,
+    'pochemu-myshechnaya-massa-vazhna-ne-tolko-dlya-relefa-no-i-dlya-uskoreniya-metabolizma':
+      pochemuMyshechnayaMassaVazhnaNekojNoITolkoDlyaRelefaNoIuDlyaUskoreniyaMetabolizma,
+    'tak-li-polezen-beg-razbiraemsya-kak-izvlech-maksimum-polzy-i-izbezhat-vreda':
+      takLiPolezenBegRazbiraemsyaKakIzvlechMaksimumPolzyIIzbezhatVreda,
+  },
 }
